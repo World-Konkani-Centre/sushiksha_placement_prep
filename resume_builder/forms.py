8b69f4dd96c2f2f25305780509842d2ee8b2bc51
swasthikcnayak <swasthik0304@gmail.com>
2021-03-30 21:57:42 +0530

    error in names sorted

--- resume_builder/forms.py
+++ resume_builder/forms.py
@@ -3,49 +3,49 @@ from django.forms import formset_factory
 
 
 class EducationForm(forms.Form):
-    name = forms.CharField(label='College/School Name', widget=forms.TextInput(attrs={'placeholder': 'Enter '
+    ed_name = forms.CharField(label='College/School Name', widget=forms.TextInput(attrs={'placeholder': 'Enter '
                                                                                                      'college/school '
                                                                                                      'name here'}))
-    course = forms.CharField(label='Course', widget=forms.TextInput(attrs={'placeholder': 'Enter the course studied'}))
-    passing_year = forms.IntegerField(label='Passing Year',
+    ed_course = forms.CharField(label='Course', widget=forms.TextInput(attrs={'placeholder': 'Enter the course studied'}))
+    ed_passing_year = forms.IntegerField(label='Passing Year',
                                       widget=forms.NumberInput(attrs={'placeholder': 'Enter the passing year'}))
-    percentage = forms.DecimalField(label='Percentage/CGPA', widget=forms.NumberInput(
+    ed_percentage = forms.DecimalField(label='Percentage/CGPA', widget=forms.NumberInput(
         attrs={'placeholder': 'Enter the the percentage/cgpa scored'}))
 
 
 class ProjectForm(forms.Form):
-    name = forms.CharField(label='Project Name',
+    p_name = forms.CharField(label='Project Name',
                            widget=forms.TextInput(attrs={'placeholder': 'Enter the project name'}))
-    start_date = forms.CharField(label='project start date')
-    end_date = forms.CharField(label='project end date')
-    description = forms.CharField(label='Project description', widget=forms.Textarea(attrs={'placeholder': 'Project '
+    p_start_date = forms.CharField(label='project start date')
+    p_end_date = forms.CharField(label='project end date')
+    p_description = forms.CharField(label='Project description', widget=forms.Textarea(attrs={'placeholder': 'Project '
                                                                                                            'Description',
                                                                                             'rows': '10',
                                                                                             'cols': '10'}))
 
 
 class SkillForm(forms.Form):
-    name = forms.CharField(label='Skill Name', widget=forms.TextInput(attrs={'placeholder': 'Your Skill'}))
-    expertise = forms.IntegerField(label='Rate your skill 1-5',
+    sk_name = forms.CharField(label='Skill Name', widget=forms.TextInput(attrs={'placeholder': 'Your Skill'}))
+    sk_expertise = forms.IntegerField(label='Rate your skill 1-5',
                                    widget=forms.NumberInput(attrs={'placeholder': 'My rating'}))
 
 
 class InternshipJobForm(forms.Form):
-    role = forms.CharField(label='Your role as an intern', widget=forms.TextInput(attrs={'placeholder': 'Your Role'}))
-    company = forms.CharField(label='Where did you intern', widget=forms.TextInput(attrs={'placeholder': 'Internship '
+    ij_role = forms.CharField(label='Your role as an intern', widget=forms.TextInput(attrs={'placeholder': 'Your Role'}))
+    ij_company = forms.CharField(label='Where did you intern', widget=forms.TextInput(attrs={'placeholder': 'Internship '
                                                                                                          'Company'}))
-    description = forms.CharField(label='give some description',
+    ij_description = forms.CharField(label='give some description',
                                   widget=forms.Textarea(
                                       attrs={'placeholder': 'Description', 'rows': '10', 'cols': '10'}))
 
 
 class AchievementForm(forms.Form):
-    name = forms.CharField(label='Something you consider an achievement',
+    a_name = forms.CharField(label='Something you consider an achievement',
                            widget=forms.TextInput(attrs={'placeholder': 'Achievement'}))
 
 
 class OtherForm(forms.Form):
-    name = forms.CharField(label='Other particular info you wish to add?',
+    o_name = forms.CharField(label='Other particular info you wish to add?',
                            widget=forms.TextInput(attrs={'placeholder': 'Others'}))
 
 
--- resume_builder/utils.py
+++ resume_builder/utils.py
@@ -1,48 +1,49 @@
 def education_is_valid(education):
     if education.is_valid():
         for form in education:
-            name = form.cleaned_data.get('name')
-            course = form.cleaned_data.get('course')
-            percentage = form.cleaned_data.get('percentage')
-            print(f" {name} -- {course}--{percentage}")
+            name = form.cleaned_data.get('ed_name')
+            course = form.cleaned_data.get('ed_course')
+            percentage = form.cleaned_data.get('ed_percentage')
+            passing_year = form.cleaned_data.get('ed_passing_year')
+            print(f" {name} -- {course}--{percentage}--{passing_year}")
 
 
 def skills_is_valid(skills):
     if skills.is_valid():
         for form in skills:
-            name = form.cleaned_data.get('name')
-            expertise = form.cleaned_data.get('expertise')
+            name = form.cleaned_data.get('sk_name')
+            expertise = form.cleaned_data.get('sk_expertise')
             print(f" {name} -- {expertise}")
 
 
 def internships_job_is_valid(internships):
     if internships.is_valid():
         for form in internships:
-            role = form.cleaned_data.get('role')
-            company = form.cleaned_data.get('company')
-            description = form.cleaned_data.get('description')
+            role = form.cleaned_data.get('ij_role')
+            company = form.cleaned_data.get('ij_company')
+            description = form.cleaned_data.get('ij_description')
             print(f" {role} -- {company} -- {description}")
 
 
 def projects_is_valid(projects):
     if projects.is_valid():
         for form in projects:
-            title = form.cleaned_data.get('title')
-            start_date = form.cleaned_data.get('start_date')
-            end_date = form.cleaned_data.get('end_date')
-            description = form.cleaned_data.get('description')
+            title = form.cleaned_data.get('p_name')
+            start_date = form.cleaned_data.get('p_start_date')
+            end_date = form.cleaned_data.get('p_end_date')
+            description = form.cleaned_data.get('p_description')
             print(f" {title} -- {start_date} -- {end_date} -- {description}")
 
 
 def achievement_is_valid(achievement):
     if achievement.is_valid():
         for form in achievement:
-            name = form.cleaned_data.get('name')
+            name = form.cleaned_data.get('a_name')
             print(f" {name}")
 
 
 def other_is_valid(other):
     if other.is_valid():
         for form in other:
-            name = form.cleaned_data.get('name')
+            name = form.cleaned_data.get('o_name')
             print(f" {name}")
