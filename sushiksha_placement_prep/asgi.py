aab42ccc642c0ad80b7fd69b2a0afd5181baa133
swasthikcnayak <swasthik0304@gmail.com>
2021-03-30 14:00:23 +0530

    creating repo for deveopment

---
+++ sushiksha_placement_prep/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sushiksha_placement_prep project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sushiksha_placement_prep.settings')
+
+application = get_asgi_application()
