40c4e20b892a202c67971d73f8e791820a9c9562
swasthikcnayak <swasthik0304@gmail.com>
2021-04-05 00:37:22 +0530

    adding styles and links

--- sushiksha_placement_prep/settings.py
+++ sushiksha_placement_prep/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_dir')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
