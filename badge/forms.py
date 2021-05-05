184e259d0c0b2687b2b4ed2e1f8e680f08cd1018
Praneeth <18praneeth@gmail.com>
2021-05-05 13:37:41 +0530

    Badge Page

--- badge/forms.py
+++ badge/forms.py
@@ -1,25 +1,32 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
-from .models import Reward
+# from django.utils.translation import gettext_lazy as _
+from .models import Reward, Badge
 from users.models import Profile
+from django.contrib.admin import widgets
+from django.db.models.functions import Lower
 
 
-class BadgeForm(forms.ModelForm):
 
-    class Meta:
-        model = Reward
-        fields = ['user', 'descritpion', 'badge']
-        help_texts = {
-            'descritpion': _('Add a short description on why you are giving the badge'),
-        }
-        labels = {
-            'descritpion': _('Message to user'),
-            'badge': _('Badge to be Awarded'),
-            'user':  _('Select the Users')
-        }
+# class BadgeForm(forms.ModelForm):
 
-    def __init__(self, *args, **kwargs):
-        super(BadgeForm, self).__init__(*args, **kwargs)
-        self.fields['user'] =  forms.ModelMultipleChoiceField(widget = forms.CheckboxSelectMultiple, queryset=Profile.objects.filter(is_mentor=False))
+#     class Meta:
+#         model = Reward
+#         fields = ['user', 'descritpion', 'badge']
+#         help_texts = {
+#             'descritpion': _('Add a short description on why you are giving the badge'),
+#         }
+#         labels = {
+#             'descritpion': _('Message to user'),
+#             'badge': _('Badge to be Awarded'),
+#             'user':  _('Select the Users')
+#         }
+
+#     def __init__(self, *args, **kwargs):
+#         super(BadgeForm, self).__init__(*args, **kwargs)
+#         self.fields['user'] =  forms.ModelMultipleChoiceField(widget = forms.CheckboxSelectMultiple, queryset=Profile.objects.filter(is_mentor=False))
 
 
+class BadgeForm(forms.Form):
+    badge = forms.ModelChoiceField(queryset=Badge.objects.all(), required=True, label='Badge to be awarded')
+    description = forms.CharField(widget=forms.Textarea(), label='Message to user')
+    profiles = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple, queryset=Profile.objects.filter(is_mentor=False).order_by(Lower('name')), required=True, label='Select the Profiles')
--- badge/views.py
+++ badge/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import BadgeForm
+from .models import Reward
 
+@login_required
 def award_badge(request):
+    if request.user.profile.is_mentor == False:
+        messages.error(request, "You don't have access to view this page :)")
+        return redirect('index')
     if request.method=='POST':
         form = BadgeForm(request.POST)
         if form.is_valid():
-            pass
-    else:
-        form = BadgeForm()
+            profiles = form.cleaned_data.get('profiles')
+            message = form.cleaned_data.get('description')
+            badge = form.cleaned_data.get('badge')
+            awarded_by = request.user.profile.name
+
+        for user in profiles:
+            Reward.objects.create(user=user, awarded_by=awarded_by, descritpion=message, badge=badge)
+        messages.success(request, f'{badge.title} Awarded to {len(profiles)} Users')
+
+        return redirect('badge')
+    
+    form = BadgeForm()
 
     context = {
         'form':form
