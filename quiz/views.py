78fdff77cda75c2a8c96430d7e60616ec602fdcf
swasthikcnayak <swasthik0304@gmail.com>
2021-05-11 19:02:02 +0530

    fixing no entry found

--- quiz/views.py
+++ quiz/views.py
@@ -269,8 +269,9 @@ class QuizTake(FormView):
 
 def superuser_only(function):
     def _inner(request, *args, **kwargs):
-        if not request.user.is_superuser:
+        if not request.user.profile.is_mentor or not request.user.is_superuser:
             raise PermissionDenied
+
         return function(request, *args, **kwargs)
 
     return _inner
