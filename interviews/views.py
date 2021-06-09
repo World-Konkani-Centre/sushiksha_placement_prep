e237def5b8ea1c5032380657d52c799fe881f0fb
Praneeth <18praneeth@gmail.com>
2021-06-09 21:47:35 +0530

    Updated buttons

--- interviews/views.py
+++ interviews/views.py
@@ -73,7 +73,7 @@ def hr_interview_details(request, intId):
 def interview_list(request):
     interviews_completed = Interview.objects.filter(
         Q(participant_2=request.user) | Q(participant_1=request.user) & Q(complete=True), type="Technical")
-    interviews_scheduled = Interview.objects.filter(complete=False, type="Technical").order_by('start_time')
+    interviews_scheduled = Interview.objects.filter(type="Technical").order_by('start_time')
     context = {
         'interviews_completed': interviews_completed,
         'interviews_scheduled': interviews_scheduled,
