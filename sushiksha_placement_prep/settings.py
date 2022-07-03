d9157335cfced83c7ec6f0a91d02c295b7e3a6bc
Praneeth <18praneeth@gmail.com>
2022-07-03 08:49:37 +0530

    crispy tags

--- sushiksha_placement_prep/settings.py
+++ sushiksha_placement_prep/settings.py
@@ -160,6 +160,29 @@ CKEDITOR_CONFIGS = {
     }
 }
 
+TINYMCE_DEFAULT_CONFIG = {
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'modern',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample | pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+}
+
 CKEDITOR_UPLOAD_PATH = os.path.join(BASE_DIR, 'media/ckeditor')
 
 # Password validation
